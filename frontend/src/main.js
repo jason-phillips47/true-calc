468a0542220a64b839dc3363c3743dd5aaf04424
Jason Phillips <jasonpve@gmail.com>
2024-06-05 00:09:40 -0700

    Update frontend to interact with backend on deployment

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import axios from 'axios';
+
+const backendURL = process.env.VUE_APP_BACKEND_URL || 'http://localhost:5000';
+
+axios.defaults.baseURL = backendURL;
+
 createApp(App).mount('#app')
