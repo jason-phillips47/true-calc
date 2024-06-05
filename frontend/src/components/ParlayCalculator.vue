eada26f6d0b346b6d0ba357d2bd30220422ffdc2
Jason Phillips <jasonpve@gmail.com>
2024-06-04 23:56:51 -0700

    Deployed on Heroku, added Presets, overhauled UI

--- frontend/src/components/ParlayCalculator.vue
+++ frontend/src/components/ParlayCalculator.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="calculator">
         <h1>Parlay EV Calculator</h1>
+        <div class="presets">
+            <button @click="togglePreset('prizepicks')">Prizepicks Presets</button>
+            <div v-if="showPrizepicks" class="preset-group">
+                <button @click="applyPreset(preset)" v-for="preset in prizepicksPresets" :key="preset.name">{{ preset.name }}</button>
+            </div>
+            <button @click="togglePreset('underdog')">Underdog Presets</button>
+            <div v-if="showUnderdog" class="preset-group">
+                <button @click="applyPreset(preset)" v-for="preset in underdogPresets" :key="preset.name">{{ preset.name }}</button>
+            </div>
+
+        </div>
         <form @submit.prevent="handleSubmit">
             <div class="columns">
                 <div class="column">
@@ -28,11 +39,11 @@
                             <input type="number" v-model.number="payout.hits" min="0" :max="legs.length" required />
                             out of {{ legs.length }} legs hit,
                             <br>
-                            <input type="radio" v-model="payout.type" :value="'payout'" /> Payout 
+                            <input type="radio" v-model="payout.type" :value="'payout'" /> Payout:
                             <input type="text" v-if="payout.type === 'payout'" v-model="payout.amount" />
                             <br>
-                            <input type="radio" v-model="payout.type" :value="'loss'" /> Lose 
-                            <input type="number" v-if="payout.type === 'loss'" v-model.number="payout.loss" min="0" step="0.0001"/> Units
+                            <input type="radio" v-model="payout.type" :value="'loss'" /> Lose Units:
+                            <input type="number" v-if="payout.type === 'loss'" v-model.number="payout.loss" step="0.1" />
                         </label>
                         <button type="button" @click="removePayout(payoutIndex)">Remove Payout</button>
                     </div>
@@ -42,7 +53,7 @@
             <button type="submit" class="calculate-button">Calculate EV</button>
         </form>
         <div v-if="result !== null" class="result">
-            <h2>Expected Value (EV): {{ result }}</h2>
+            <h2>Expected Value (EV): {{ result.toFixed(6) }}</h2>
         </div>
     </div>
 </template>
@@ -57,9 +68,149 @@ export default {
                 { hitRate: 50 }
             ],
             payouts: [
-                { hits: 1, type: 'payout', amount: "+100", loss: 0 }
+                { hits: 1, total: 1, amount: 100 }
             ],
-            result: null
+            result: null,
+            showPrizepicks: false,
+            showUnderdog: false,
+            prizepicksPresets: [
+                {
+                    name: "Prizepicks 2 power",
+                    legs: Array(2).fill({ hitRate: 50 }),
+                    payouts: [
+                        { hits: 2, type: 'payout', amount: "+200", loss: 0 }                    
+                    ]
+                },
+                {
+                    name: "Prizepicks 3 flex",
+                    legs: Array(3).fill({ hitRate: 50 }),
+                    payouts: [
+                        { hits: 3, type: 'payout', amount: "+125", loss: 0 },
+                        { hits: 2, type: 'payout', amount: "-400", loss: 0 }
+                    ]
+                },
+                {
+                    name: "Prizepicks 3 power",
+                    legs: Array(3).fill({ hitRate: 50 }),
+                    payouts: [
+                        { hits: 3, type: 'payout', amount: "+400", loss: 0 }                    
+                    ]
+                },
+                {
+                    name: "Prizepicks 4 flex",
+                    legs: Array(4).fill({ hitRate: 50 }),
+                    payouts: [
+                        { hits: 4, type: 'payout', amount: "+400", loss: 0 },
+                        { hits: 3, type: 'payout', amount: "-200", loss: 0 }
+                    ]
+                },
+                {
+                    name: "Prizepicks 4 power",
+                    legs: Array(4).fill({ hitRate: 50 }),
+                    payouts: [
+                        { hits: 4, type: 'payout', amount: "+900", loss: 0 }
+                    ]
+                },
+                {
+                    name: "Prizepicks 5 flex",
+                    legs: Array(5).fill({ hitRate: 50 }),
+                    payouts: [
+                        { hits: 5, type: 'payout', amount: "+900", loss: 0 },
+                        { hits: 4, type: 'payout', amount: "+100", loss: 0 },
+                        { hits: 3, type: 'loss', amount: "0", loss: 0.6 }
+                    ]
+                },
+                {
+                    name: "Prizepicks 6 flex",
+                    legs: Array(6).fill({ hitRate: 50 }),
+                    payouts: [
+                        { hits: 6, type: 'payout', amount: "+2400", loss: 0 },
+                        { hits: 5, type: 'payout', amount: "+100", loss: 0 },
+                        { hits: 4, type: 'loss', amount: "0", loss: 0.6 }
+                    ]
+                }
+            ],
+            underdogPresets: [
+                {
+                    name: "Underdog 2 power",
+                    legs: Array(2).fill({ hitRate: 50 }),
+                    payouts: [
+                        { hits: 2, type: 'payout', amount: "+200", loss: 0 }                    
+                    ]
+                },
+                {
+                    name: "Underdog 3 flex",
+                    legs: Array(3).fill({ hitRate: 50 }),
+                    payouts: [
+                        { hits: 3, type: 'payout', amount: "+200", loss: 0 },
+                        { hits: 2, type: 'payout', amount: "0", loss: 0 }                    
+                    ]
+                },
+                {
+                    name: "Underdog 3 power",
+                    legs: Array(3).fill({ hitRate: 50 }),
+                    payouts: [
+                        { hits: 3, type: 'payout', amount: "+500", loss: 0 }                    
+                    ]
+                },
+                {
+                    name: "Underdog 4 flex",
+                    legs: Array(4).fill({ hitRate: 50 }),
+                    payouts: [
+                        { hits: 4, type: 'payout', amount: "+500", loss: 0 },
+                        { hits: 3, type: 'payout', amount: "-200", loss: 0 }                    
+                    ]
+                },
+                {
+                    name: "Underdog 4 power",
+                    legs: Array(4).fill({ hitRate: 50 }),
+                    payouts: [
+                        { hits: 4, type: 'payout', amount: "+900", loss: 0 }                    
+                    ]
+                },
+                {
+                    name: "Underdog 5 flex",
+                    legs: Array(5).fill({ hitRate: 50 }),
+                    payouts: [
+                        { hits: 5, type: 'payout', amount: "+900", loss: 0 },
+                        { hits: 4, type: 'payout', amount: "+150", loss: 0 }                   
+                    ]
+                },
+                {
+                    name: "Underdog 5 power",
+                    legs: Array(5).fill({ hitRate: 50 }),
+                    payouts: [
+                        { hits: 5, type: 'payout', amount: "+1900", loss: 0 }             
+                    ]
+                },
+                {
+                    name: "Underdog 6 flex",
+                    legs: Array(6).fill({ hitRate: 50 }),
+                    payouts: [
+                        { hits: 6, type: 'payout', amount: "+2400", loss: 0 },
+                        { hits: 5, type: 'payout', amount: "+160", loss: 0 },
+                        { hits: 4, type: 'loss', amount: "0", loss: 0.75 }                    
+                    ]
+                },
+                {
+                    name: "Underdog 7 flex",
+                    legs: Array(7).fill({ hitRate: 50 }),
+                    payouts: [
+                        { hits: 7, type: 'payout', amount: "+3900", loss: 0 },
+                        { hits: 6, type: 'payout', amount: "+175", loss: 0 },
+                        { hits: 5, type: 'loss', amount: "0", loss: 0.5 }                   
+                    ]
+                },
+                {
+                    name: "Underdog 8 flex",
+                    legs: Array(8).fill({ hitRate: 50 }),
+                    payouts: [
+                        { hits: 8, type: 'payout', amount: "+7900", loss: 0 },
+                        { hits: 7, type: 'payout', amount: "+200", loss: 0 },
+                        { hits: 6, type: 'payout', amount: "0", loss: 0 }                    
+                    ]
+                }
+            ]
         };
     },
     methods: {
@@ -74,6 +225,19 @@ export default {
         },
         removePayout(index) {
             this.payouts.splice(index, 1);
+        },
+        applyPreset(preset) {
+            this.legs = JSON.parse(JSON.stringify(preset.legs));
+            this.payouts = JSON.parse(JSON.stringify(preset.payouts));
+        },
+        togglePreset(group) {
+            if (group === 'prizepicks') {
+                this.showPrizepicks = !this.showPrizepicks;
+                if (this.showPrizepicks) this.showUnderdog = false;
+            } else if (group === 'underdog') {
+                this.showUnderdog = !this.showUnderdog;
+                if (this.showUnderdog) this.showPrizepicks = false;
+            }
         },
         parseOdds(odds) {
             let parsedOdds = parseFloat(odds);
@@ -107,7 +271,7 @@ export default {
 
 body {
     font-family: 'Roboto', sans-serif;
-    background: #6c757d;
+    background: #cadae8;
 }
 
 .calculator {
@@ -122,6 +286,52 @@ body {
 h1 {
     text-align: center;
     margin-bottom: 20px;
+}
+
+.presets {
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+}
+
+
+
+.presets button {
+    display: block;
+    width: 300px;
+    font-size: large;
+    margin: 10px auto;
+    background: #007BFF;
+    color: #fff;
+    border: none;
+    padding: 8px 12px;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+.preset-group {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 10px;
+}
+
+.preset-group button {
+    display: block;
+    width: 200px;
+    margin: 10px auto;
+    background: #007BFF;
+    font-size: small;
+    color: #fff;
+    border: none;
+    padding: 8px 12px;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+.presets button:hover {
+    background: #0056b3;
 }
 
 .columns {
@@ -164,7 +374,7 @@ button {
 }
 
 button[type="button"] {
-    background: #6c757d;
+    background: #007BFF;
 }
 
 button:hover {
@@ -172,7 +382,7 @@ button:hover {
 }
 
 button[type="button"]:hover {
-    background: #5a6268;
+    background: #0056b3;
 }
 
 .calculate-button {
