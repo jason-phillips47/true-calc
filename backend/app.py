490645af7a055962fd16b5e9f83c753230d4673e
Jason Phillips <jasonpve@gmail.com>
2024-06-05 00:12:44 -0700

    Configure CORS in Flask

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
 import numpy as np
 import itertools
 
 app = Flask(__name__, static_folder='../frontend/dist', static_url_path='/')
+CORS(app)
 
 @app.route('/calculate_ev', methods=['POST'])
 def calculate_ev():
